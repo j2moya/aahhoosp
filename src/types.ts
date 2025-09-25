22c37ea4b6d1c7fd5c3947e08be44a4a6240624d
JOSE MOYA <josemoya@JOSEs-Mac-mini.attlocal.net>
2025-09-25 15:14:49 -0400

    Initial commit: Project cleaned and structured for deployment

---
+++ src/types.ts
@@ -0,0 +1,57 @@
+export type Language = 'en' | 'es';
+
+export interface Service {
+  title: string;
+  description: string;
+}
+
+export interface ExternalLink {
+  title: string;
+  url: string;
+  description: string;
+  icon: string;
+}
+
+export interface OfficeLocation {
+    city: string;
+    addresses: string[];
+}
+
+export interface Associate {
+    name: string;
+    location: string;
+}
+
+export interface Content {
+  [key: string]: {
+    tagline: string;
+    heroSubtitle: string;
+    aboutTitle: string;
+    aboutText: string;
+    servicesTitle: string;
+    services: Service[];
+    locationsTitle: string;
+    locationsSubtitle: string;
+    locations: OfficeLocation[];
+    associatesTitle: string;
+    associatesSubtitle: string;
+    associates: Associate[];
+    externalLinksTitle: string;
+    streamTitle: string;
+    streamText: string;
+    booksTitle: string;
+    booksText: string;
+    moreLinksTitle: string;
+    moreLinksSubtitle: string;
+    moreLinks: ExternalLink[];
+    exploreMoreTitle: string;
+    exploreMoreLinks: ExternalLink[];
+    contactTitle: string;
+    investmentPrice: string;
+    bookingLink: string;
+    callToAction: string;
+    loginText: string;
+    registerText: string;
+    footerRights: string;
+  };
+}
